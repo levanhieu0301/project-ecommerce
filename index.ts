5be89b254f8633ac4f0a330991b2c4bdd8d25d07
lvh0301 <levanhieu0301@gmail.com>
2026-07-17 22:37:03 +0700

    Ghép code Frontend cho Trang chủ (bên client)

--- index.ts
+++ index.ts
@@ -1,10 +1,19 @@
 import express, { Request, Response } from "express"
+import path from "path"
 const app = express()
 const port = 5000
+// Tích hợp giao diện pug
+app.set('views', path.join(__dirname, "views"))
+app.set('view engine', 'pug')
+// Nhúng file tĩnh 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+  res.render("client/pages/index", {
+    title: "Trang chủ"
+  })
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
