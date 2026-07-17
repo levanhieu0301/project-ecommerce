394f143dcf6829fd89b368dc5526b816b102176a
lvh0301 <levanhieu0301@gmail.com>
2026-07-17 23:57:56 +0700

    Sửa Startbar bên admin

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express"
 import path from "path"
+import { pathAdmin } from "./configs/variable.config"
 const app = express()
 const port = 5000
 // Tích hợp giao diện pug
@@ -7,12 +8,17 @@ app.set('views', path.join(__dirname, "views"))
 app.set('view engine', 'pug')
 // Nhúng file tĩnh 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.locals.pathAdmin = pathAdmin
+
+
 // Router client
 import clientRoute from "./routes/client/index.route"
 app.use('/', clientRoute)
 // Router admin
 import adminRoute from "./routes/admin/index.route"
-app.use('/admin', adminRoute)
+app.use(`/${pathAdmin}`, adminRoute)
+
 
 
 app.listen(port, () => {
