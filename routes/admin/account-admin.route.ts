915afdcc5f63534e0499c02b0e3b99c7cd34cdfb
lvh0301 <levanhieu0301@gmail.com>
2026-08-04 15:30:59 +0700

    Trang tạo tài khoản quản trị

---
+++ routes/admin/account-admin.route.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+const router = Router();
+import * as accountAdminController from "../../controllers/admin/account-admin.controller"
+import * as accountAdminValidate from "../../validates/admin/account-admin.validate"
+import multer from "multer";
+const upload = multer();
+
+router.get('/create', accountAdminController.createAccountAdmin)
+router.post('/create', upload.none(), accountAdminValidate.createPost, accountAdminController.createAccountAdminPost)
+
+export default router;
--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -5,11 +5,12 @@ import articleRoute from "./article.route"
 import helperRoute from "./helper.route"
 import fileManagerRoute from "./file-manager.route"
 import roleRoute from "./role.route"
+import accountAdminRoute from "./account-admin.route"
 
 router.use('/dashboard', dashboardRoute)
 router.use('/article', articleRoute)
 router.use('/helper', helperRoute)
 router.use('/file-manager', fileManagerRoute)
 router.use('/role', roleRoute)
-
+router.use('/account-admin', accountAdminRoute)
 export default router;
