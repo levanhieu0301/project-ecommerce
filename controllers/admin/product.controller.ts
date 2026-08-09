38e15e699e6de77b69eba343435bf0eed5fcd62b
lvh0301 <levanhieu0301@gmail.com>
2026-08-09 16:30:35 +0700

    Tính năng phân trang và tìm kiếm danh mục sản phẩm

--- controllers/admin/product.controller.ts
+++ controllers/admin/product.controller.ts
@@ -4,9 +4,45 @@ import slugify from "slugify"
 import { treeCategory } from "../../helpers/treeCategory.helper"
 
 export const categoryProduct = async (req: Request, res: Response) => {
-  const recordList: any = await CategoryProduct.find({
+  const find : {
+    deleted: boolean,
+    search?: RegExp
+  } = {
     deleted: false,
-  })
+  }
+   // Phân trang
+  const limitItems = 20;
+  let page = 1;
+  if(req.query.page && parseInt(`${req.query.page}`) > 0) {
+    page = parseInt(`${req.query.page}`);
+  }
+  const totalRecord = await CategoryProduct.countDocuments(find);
+  const totalPage = Math.ceil(totalRecord/limitItems);
+  const skip = (page - 1) * limitItems;
+  const pagination = {
+    totalRecord: totalRecord,
+    totalPage: totalPage,
+    skip: skip
+  };
+  // Hết Phân trang
+  // Tìm kiếm
+  if(req.query.keyword) {
+    const keyword = slugify(`${req.query.keyword}`, {
+      replacement: " ",
+      lower: true
+    });
+    const keywordRegex = new RegExp(keyword, "i");
+    find.search = keywordRegex;
+  }
+
+  const recordList: any = await CategoryProduct
+    .find(find)
+    .sort({
+      createdAt: "desc"
+    })
+    .limit(limitItems)
+    .skip(skip)
+
   for(const record of recordList){
     if(record.parent){
       const parent = await CategoryProduct.findOne({
@@ -19,7 +55,8 @@ export const categoryProduct = async (req: Request, res: Response) => {
   }
   res.render("admin/pages/product-category", {
     pageTitle: "Danh mục sản phẩm",
-    recordList: recordList
+    recordList: recordList,
+    pagination: pagination
   })
 }
 export const categoryProductCreate = async (req: Request, res: Response) => {
