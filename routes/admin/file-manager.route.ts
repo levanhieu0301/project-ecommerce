971d581360528f16ef5f91a4b3e3bd694b86e08d
lvh0301 <levanhieu0301@gmail.com>
2026-08-12 16:30:38 +0700

    Fix lỗi font chữ khi upload file

--- routes/admin/file-manager.route.ts
+++ routes/admin/file-manager.route.ts
@@ -2,7 +2,20 @@ import { Router } from "express";
 const router = Router();
 import * as fileManagerController from "../../controllers/admin/file-manager.controller"
 import multer from "multer";
-const upload = multer();
+// const upload = multer();
+// Dùng memoryStorage để giữ file trong buffer
+const storage = multer.memoryStorage();
+
+// Fix lỗi font tiếng Việt trong tên file (multer mặc định Latin1)
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    // Ép originalname về UTF-8
+    file.originalname = Buffer.from(file.originalname, 'latin1').toString('utf8');
+    cb(null, true);
+  }
+});
+
 
 
 router.get('/', fileManagerController.fileManager)
