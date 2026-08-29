c94437e8bdb617c9771ae1b7984aa4c7f7f3558f
lvh0301 <levanhieu0301@gmail.com>
2026-08-30 01:10:35 +0700

    Đăng ký tài khoản người dùng

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -6,6 +6,7 @@ import productRoute from "./product.route"
 import cartRoutes from "./cart.route";
 import compareRoutes from "./compare.route";
 import wishlistRoutes from "./wishlist.route";
+import authRoutes from "./auth.route";
 import * as categoryMiddleware from "../../middlewares/client/category.middleware"
 import * as attributeMiddleware from "../../middlewares/client/attribute.middleware"
 
@@ -17,6 +18,7 @@ router.use('/product', productRoute)
 router.use('/cart', cartRoutes);
 router.use('/compare', compareRoutes);
 router.use('/wishlist', wishlistRoutes);
+router.use('/auth', authRoutes);
 
 
 
