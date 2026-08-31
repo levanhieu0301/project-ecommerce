445ffdb70be2fae308a0ffb5eb8a9d525e06b48b
lvh0301 <levanhieu0301@gmail.com>
2026-08-31 16:15:20 +0700

    Trang Tài khoản cá nhân (Tạo khung giao diện)

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -7,6 +7,7 @@ import cartRoutes from "./cart.route";
 import compareRoutes from "./compare.route";
 import wishlistRoutes from "./wishlist.route";
 import authRoutes from "./auth.route";
+import dashboardRoutes from "./dashboard.route";
 import * as categoryMiddleware from "../../middlewares/client/category.middleware"
 import * as attributeMiddleware from "../../middlewares/client/attribute.middleware"
 import * as authMiddleware from "../../middlewares/client/auth.middleware";
@@ -21,6 +22,7 @@ router.use('/cart', cartRoutes);
 router.use('/compare', compareRoutes);
 router.use('/wishlist', wishlistRoutes);
 router.use('/auth', authRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 
 
