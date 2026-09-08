4a9d67e1570f3e68bde4e674557338c0643f62f1
lvh0301 <levanhieu0301@gmail.com>
2026-09-09 01:47:28 +0700

    Quản lý API cổng thanh toán (bên admin)

--- routes/admin/setting.route.ts
+++ routes/admin/setting.route.ts
@@ -6,5 +6,9 @@ const router = Router();
 router.get('/api-shipping', settingController.apiShipping);
 
 router.patch('/api-shipping', settingController.apiShippingPatch);
+router.get('/api-payment', settingController.apiPayment);
+
+router.patch('/api-payment', settingController.apiPaymentPatch);
+
 
 export default router;
