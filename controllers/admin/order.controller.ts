6b3fdf5e551e9bea0d4bc9373cf65a6e071f11e8
lvh0301 <levanhieu0301@gmail.com>
2026-09-09 10:35:35 +0700

    Export đơn hàng qua Excel/CSV (bên admin)

--- controllers/admin/order.controller.ts
+++ controllers/admin/order.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import Order from '../../models/order.model';
 import { pathAdmin } from '../../configs/variable.config';
+import { Parser } from 'json2csv';
 
 export const list = async (req: Request, res: Response) => {
   const find: {
@@ -121,3 +122,22 @@ export const editPatch = async (req: Request, res: Response) => {
     });
   }
 };
+export const exportCSV = async (req: Request, res: Response) => {
+  try {
+    const orderList = await Order.find().lean();
+
+    // Chuyển JSON -> CSV
+    const parser = new Parser({
+      delimiter: ";"
+    });
+    let csv = parser.parse(orderList);
+    csv = "\ufeff" + csv;
+
+    // Gửi file CSV về client
+    res.header("Content-Type", "text/csv");
+    res.attachment("orders.csv");
+    res.send(csv);
+  } catch (err) {
+    console.error("Export CSV error:", err);
+  }
+}
--- routes/admin/order.route.ts
+++ routes/admin/order.route.ts
@@ -8,6 +8,6 @@ router.get('/list', orderController.list);
 router.get('/edit/:id', orderController.edit);
 
 router.patch('/edit/:id', orderController.editPatch);
-
+router.get('/export/csv', orderController.exportCSV);
 
 export default router;
