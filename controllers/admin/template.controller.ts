1386f6522bf6a9edefe7547e742bd211af12026e
lvh0301 <levanhieu0301@gmail.com>
2026-09-11 04:16:55 +0700

    Hiển thị dữ liệu ra danh sách

--- controllers/admin/template.controller.ts
+++ controllers/admin/template.controller.ts
@@ -5,8 +5,14 @@ import slugify from "slugify";
 import Template from "../../models/template.model";
 
 export const list = async (req: Request, res: Response) => {
+  
+  const recordList = await Template.find({
+    deleted: false
+  })
+  
   res.render("admin/pages/template-list", {
-    pageTitle: "Quản lý template"
+    pageTitle: "Quản lý template",
+    recordList: recordList
   });
 }
 export const create = async (req: Request, res: Response) => {
