eb0adc86c4f2c66915ef2ea028b0448cdbf77352
lvh0301 <levanhieu0301@gmail.com>
2026-09-10 16:38:01 +0700

    Cấu hình robots.txt

--- routes/client/home.route.ts
+++ routes/client/home.route.ts
@@ -5,6 +5,7 @@ import * as homeController from "../../controllers/client/home.controller"
 
 router.get('/', homeController.home)
 router.get('/sitemap.xml', homeController.sitemap);
+router.get('/robots.txt', homeController.robots);
 
 
 export default router;
