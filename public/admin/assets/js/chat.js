2ecb88484f749ae7b2cc760878ffe094c52ebd37
lvh0301 <levanhieu0301@gmail.com>
2026-09-15 23:32:23 +0700

    Hiển thị tin nhắn realtime cho admin

--- public/admin/assets/js/chat.js
+++ public/admin/assets/js/chat.js
@@ -8,6 +8,8 @@ const formChat = document.querySelector("[form-chat]")
 if(formChat){
   const inputContent = formChat.querySelector("[input-content]");
   const buttonSend = formChat.querySelector("[button-send]");
+  const chatRoomId = document.querySelector(".chat-box-right").getAttribute("chat-room-id")
+  const chatDetail = document.querySelector(".chat-detail")
   buttonSend.addEventListener("click", () => {
     const content = inputContent.value.trim()
     if(content){
@@ -19,6 +21,22 @@ if(formChat){
   })
   // Nhận tin nhắn từ server - lắng nghe từ sever
   socket.on("SERVER_SEND_MESSAGE", (data) => {
-    console.log(data);
+    // Kiểm tra đúng phòng chat không
+    if(chatRoomId == data.roomId){
+      const elementMessage = document.createElement("div");
+       if(data.senderRole === 'user') {
+        elementMessage.classList.add("d-flex");
+      } else {
+        elementMessage.classList.add("d-flex", "flex-row-reverse");
+      }
+      elementMessage.innerHTML = `
+        <div class="chat-box w-100 ${data.senderRole === 'user' ? '' : 'reverse'}">
+          <div class="user-chat">
+            <p>${data.content}</p>
+          </div>
+        </div>
+      `;
+      chatDetail.appendChild(elementMessage)
+    }
   });
 }
