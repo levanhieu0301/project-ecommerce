f708369aa0d3c3cfcaccab381862e375c71665b2
lvh0301 <levanhieu0301@gmail.com>
2026-09-20 10:08:03 +0700

    Gửi file (bên client)

--- routes/client/chat.route.ts
+++ routes/client/chat.route.ts
@@ -1,9 +1,17 @@
 
 import { Router } from "express";
 import * as chatController from "../../controllers/client/chat.controller";
+import multer from "multer";
+const upload = multer();
 
 const router = Router();
 
 router.get('/messages', chatController.messages);
+router.post(
+  '/upload', 
+  upload.array("files"), 
+  chatController.uploadPost
+);
+
 
 export default router;
